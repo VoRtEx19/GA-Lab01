0b36b9e910d795952165997b6c8aae85e70adebe
VoRtEx19 <lasvetlichnyy@edu.hse.ru>
2023-10-01 15:40:45 +0300

    v 1.0 activity_main.xml: - added my name and group to textview - change design of textview - added button "Tap to say hello!" MainActivity.java: - added onClick check that sets textview to "Hello!" and button to unclickable and invisible strings.xml: - added some string resources

--- app/src/main/java/com/example/lab01/MainActivity.java
+++ app/src/main/java/com/example/lab01/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.lab01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button hello;
+    TextView main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hello = findViewById(R.id.hello_button);
+        main = findViewById(R.id.main_textView);
+
+        hello.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        main.setText(R.string.hello_string);
+        hello.setClickable(false);
+        hello.setVisibility(View.INVISIBLE);
     }
 }
